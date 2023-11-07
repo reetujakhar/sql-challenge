20fc3f7ed432fb02e196b3f5688a60a8a107011d
reetujakhar <ritu.jakhar.8@gmail.com>
2023-11-07 17:39:19 -0500

    moved output files to EmployeeSQL folder

---
+++ EmployeeSQL/queries.sql
@@ -0,0 +1,69 @@
+----List the employee number, last name, first name, sex, and salary of each employee.
+
+select employees.emp_no, last_name, first_name, sex, salary
+from employees
+join salaries 
+on employees.emp_no = salaries.emp_no;
+
+
+-----List the first name, last name, and hire date for the employees who were hired in 1986.
+
+select first_name, last_name, hire_date
+from employees
+where hire_date between '01/01/1986' and '12/31/1986';
+
+
+-----List the manager of each department along with their department number, department name, employee number, last name, and first name.
+
+select dept_manager.dept_no, dept_name, employees.emp_no, last_name, first_name
+from dept_manager
+join departments
+on departments.dept_no = dept_manager.dept_no
+join employees
+on dept_manager.emp_no = employees.emp_no;
+
+
+-----List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
+
+select departments.dept_no, dept_emp.emp_no, last_name, first_name, dept_name
+from departments
+join dept_emp
+on departments.dept_no = dept_emp.dept_no
+join employees
+on dept_emp.emp_no = employees.emp_no;
+
+
+------List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
+
+select first_name, last_name, sex
+from employees
+where first_name = 'Hercules' and last_name like 'B%';
+
+
+-----List each employee in the Sales department, including their employee number, last name, and first name.
+
+select dept_emp.dept_no as department,
+employees.emp_no as "employee number", last_name, first_name from employees
+join dept_emp
+on employees.emp_no = dept_emp.emp_no
+where dept_no in ( select dept_no from departments where dept_name = 'Sales');
+
+
+------List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+select dept_emp.dept_no as department, dept_name as "department name",
+employees.emp_no as "employee number", last_name, first_name from employees
+join dept_emp
+on employees.emp_no = dept_emp.emp_no
+join departments
+on departments.dept_no = dept_emp.dept_no
+where departments.dept_no in ( select dept_no from departments 
+				  where dept_name in ('Sales','Development'));
+				  
+				  
+-----List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
+
+
+select count(*),last_name from employees
+group by last_name
+order by count(*) desc;
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # sql-challenge
+
+Created EmployeeSQL folder and added the sql files in that. Created the ERD diagram for the tables and exported the png image to EmployeeSQL folder.
+Also added the table schemata and queries solutions in .sql files in EmployeeSQL folder.
+Resources contains the data files.
+Executed all 8 queries.
